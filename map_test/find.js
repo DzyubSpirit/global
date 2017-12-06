b312c5930648fd33549fc4d613f94b38fe471883
Vlad Dziuba <dzyubavlad@gmail.com>
2017-12-06 09:49:08 +0200

    Improve searching speed

--- map_test/find.js
+++ map_test/find.js
@@ -9,28 +9,22 @@ const searchObject = {
 };
 
 const findObjectByIndex = (searchObject) => {
-  let resultBuffer;
   const index = createIndex(searchObject);
-  mainMap.forEach((obj, key) => {
-    if (key === index) resultBuffer = bufferMap.get(obj);
-  });
+  const resultBuffer = mainMap.get(index);
   return resultBuffer;
 };
 
 const findObjectByObjectFields = (searchObject) => {
-  let resultBuffer;
-  let foundAll = true;
-
-  mainMap.forEach((obj) => {
+  for (let [_, obj] of mainMap) {
+    let foundAll = true;
     for (const key in searchObject) {
       if (obj[key] !== searchObject[key]) {
         foundAll = false;
         break;
       }
-      if (foundAll) resultBuffer = bufferMap.get(obj);
-      foundAll = true;
+      if (foundAll) return bufferMap.get(obj);
     }
-  });
-  return resultBuffer;
+  }
+  return null;
 };
 
